47027ed2e626d54874b795db0d4062b7d7bda394
Federico Cendali <federicocendali@gmail.com>
2023-07-06 23:18:05 -0300

    [feature] - create auth stack and add logic to show auth screen

--- src/constants/firebase/index.js
+++ src/constants/firebase/index.js
@@ -1,1 +1,2 @@
 export const FIREBASE_REALTIME_DB_URL = "https://bread-store-8bf19-default-rtdb.firebaseio.com/";
+export const FIREBASE_API_KEY = "AIzaSyA4NrFsy7T6CGXLNV_NpoHw86VG0xORa7k";
---
+++ src/screens/auth/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+import { COLORS } from "../../constants";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: COLORS.white,
+  },
+});
